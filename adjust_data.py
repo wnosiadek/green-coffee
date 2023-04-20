b7ad9801385d672dc0e6b4d25b9592ee36d5fab8
wnosiadek <129866348+wnosiadek@users.noreply.github.com>
2023-04-20 20:26:43 +0200

    Update adjust_data.py
    Additional comments
    Changing xlsx file format to csv

--- adjust_data.py
+++ adjust_data.py
@@ -1,11 +1,11 @@
-# read the xlsx data
+# read csv data
 # adjust the data where necessary
-# write the data to a new xlsx
+# write the data to a new csv file
 
 import pandas
 
 # read data from the provided file
-data = pandas.read_excel('data.xlsx')
+data = pandas.read_csv('data.csv')
 
 # column labels
 print(data.columns)
@@ -24,21 +24,26 @@ data.drop(columns=['Price USD/kg', 'Price EUR/kg'], inplace=True)
 print(data.columns)
 # ['Weight', 'Coffee', 'Process', 'Score', 'Profile', 'Bags', 'Price']
 
-# convert strings to ints, floats and NaNs where necessary
+# adjust the data
 
+# remove units from weights
+# convert numeric strings to integers
 data['Weight'] = data['Weight'].str.removesuffix(' kg')
 data['Weight'] = pandas.to_numeric(data['Weight'])
-
+# replace commas with periods in scores
+# convert numeric strings to floats
+# replace alphabetic strings and dashes with nans
 data['Score'] = data['Score'].astype(str)
 data['Score'] = data['Score'].str.replace(',', '.')
 data['Score'] = pandas.to_numeric(data['Score'], errors='coerce')
-
+# replace dashes with nans
 data['Profile'] = data['Profile'].mask(data['Profile'] == '-')
-
+# ensure that bags values are integers
 data['Bags'] = pandas.to_numeric(data['Bags'])
-
+# replace commas with periods in prices
+# convert numeric strings to floats
 data['Price'] = data['Price'].str.replace(',', '.')
 data['Price'] = pandas.to_numeric(data['Price'])
 
 # write the data to a new file
-data.to_excel('adjusted_data.xlsx')
+data.to_csv('adjusted_data.csv')
