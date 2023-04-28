1acb66b5693ad158ff4e166c18decef416db8e93
wnosiadek <129866348+wnosiadek@users.noreply.github.com>
2023-04-28 12:25:22 +0200

    Update adjust_data.py
    Change a script to a module so that it can be used easier for new files

--- adjust_data.py
+++ adjust_data.py
@@ -1,49 +1,49 @@
-# read csv data
-# adjust the data where necessary
-# write the data to a new csv file
+def adjust_data(file_name):
 
-import pandas
+  # read csv data
+  # adjust the data where necessary
+  # write the data to a new csv file
 
-# read data from the provided file
-data = pandas.read_csv('data.csv')
+  import pandas
 
-# column labels
-print(data.columns)
-# ['Weight', 'Coffee', 'Process', 'SCA score', 'Sensory profile',
-#        'Approx. no of bags SPOT', 'Cena PLN/kg netto', 'Price USD/kg',
-#        'Price EUR/kg']
+  # read data from the provided file
+  data = pandas.read_csv(file_name)
 
-# rename column labels
-data.rename(columns={'SCA score': 'Score', 'Sensory profile': 'Profile',
-                     'Approx. no of bags SPOT': 'Bags',
-                     'Cena PLN/kg netto': 'Price'}, inplace=True)
-# drop irrelevant columns
-data.drop(columns=['Price USD/kg', 'Price EUR/kg'], inplace=True)
+  # column labels
+  print(data.columns)
+  # ['Weight', 'Coffee', 'Process', 'SCA score', 'Sensory profile', 'Approx. no of bags SPOT',
+  #  'Cena PLN/kg netto', 'Price USD/kg', 'Price EUR/kg']
 
-# new column labels
-print(data.columns)
-# ['Weight', 'Coffee', 'Process', 'Score', 'Profile', 'Bags', 'Price']
+  # rename column labels
+  data.rename(columns={'SCA score': 'Score', 'Sensory profile': 'Profile', 'Approx. no of bags SPOT': 'Bags',
+                       'Cena PLN/kg netto': 'Price'}, inplace=True)
+  # drop irrelevant columns
+  data.drop(columns=['Price USD/kg', 'Price EUR/kg'], inplace=True)
 
-# adjust the data
+  # new column labels
+  print(data.columns)
+  # ['Weight', 'Coffee', 'Process', 'Score', 'Profile', 'Bags', 'Price']
 
-# remove units from weights
-# convert numeric strings to integers
-data['Weight'] = data['Weight'].str.removesuffix(' kg')
-data['Weight'] = pandas.to_numeric(data['Weight'])
-# replace commas with periods in scores
-# convert numeric strings to floats
-# replace alphabetic strings and dashes with nans
-data['Score'] = data['Score'].astype(str)
-data['Score'] = data['Score'].str.replace(',', '.')
-data['Score'] = pandas.to_numeric(data['Score'], errors='coerce')
-# replace dashes with nans
-data['Profile'] = data['Profile'].mask(data['Profile'] == '-')
-# ensure that bags values are integers
-data['Bags'] = pandas.to_numeric(data['Bags'])
-# replace commas with periods in prices
-# convert numeric strings to floats
-data['Price'] = data['Price'].str.replace(',', '.')
-data['Price'] = pandas.to_numeric(data['Price'])
+  # adjust the data
 
-# write the data to a new file
-data.to_csv('adjusted_data.csv')
+  # remove units from weights
+  # convert numeric strings to integers
+  data['Weight'] = data['Weight'].str.removesuffix(' kg')
+  data['Weight'] = pandas.to_numeric(data['Weight'])
+  # replace commas with periods in scores
+  # convert numeric strings to floats
+  # replace alphabetic strings and dashes with nans
+  data['Score'] = data['Score'].astype(str)
+  data['Score'] = data['Score'].str.replace(',', '.')
+  data['Score'] = pandas.to_numeric(data['Score'], errors='coerce')
+  # replace dashes with nans
+  data['Profile'] = data['Profile'].mask(data['Profile'] == '-')
+  # ensure that bags values are integers
+  data['Bags'] = pandas.to_numeric(data['Bags'])
+  # replace commas with periods in prices
+  # convert numeric strings to floats
+  data['Price'] = data['Price'].str.replace(',', '.')
+  data['Price'] = pandas.to_numeric(data['Price'])
+
+  # write the data to a new file
+  data.to_csv('adjusted_' + file_name)
