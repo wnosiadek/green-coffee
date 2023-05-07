bc52ebd8ab1417960f3fc7ed58730de5d34593e6
wnosiadek <129866348+wnosiadek@users.noreply.github.com>
2023-05-07 21:34:48 +0200

    Add files via upload
    Adding a module for origin simplification

---
+++ origins.py
@@ -0,0 +1,48 @@
+# simplify possible origins
+# take into account continents rather than countries
+# change alphabetical data into numerical data
+
+# possible continents
+africa = ['Burundi', 'Cameroon', "Côte d'Ivoire", 'Democratic Republic of Congo', 'Ethiopia', 'Guinea', 'Kenya',
+          'Madagascar', 'Rwanda', 'Tanzania', 'Togo', 'Uganda']
+asia = ['India', 'Indonesia', "Lao People's Democratic Republic", 'Papua New Guinea', 'Philippines', 'Thailand',
+        'Vietnam', 'Yemen']
+c_america = ['Costa Rica', 'Cuba', 'Dominican Republic', 'El Salvador', 'Guatemala', 'Haiti', 'Honduras', 'Mexico',
+             'Nicaragua', 'Panama']
+s_america = ['Bolivia', 'Brazil', 'Colombia', 'Ecuador', 'Peru', 'Venezuela']
+
+# assign an integer id to each continent, and then to each country on that continent
+origins_map = {country: continent_id
+               for continent_id, continent in enumerate([africa, asia, c_america, s_america])
+               for country in continent}
+printable_origins_map = {continent: continent_id for continent_id, continent
+                         in enumerate(['Africa', 'Asia & Oceania', 'Mexico & Central America', 'South America'])}
+
+if __name__ == 'main':
+    print(printable_origins_map)
+    # {'Africa': 0, 'Asia & Oceania': 1, 'Mexico & Central America': 2, 'South America': 3}
+
+
+# use the above to transform the data
+def simplify_origins(features, origins):
+
+    # extract countries from coffee names
+    origins = origins.str.partition()[0]
+    print(origins.unique())
+    # ['Brazil' 'Colombia' 'Costa' 'Dom.' 'Ethiopia' 'Honduras' 'Indonesia' 'Kenya' 'Malawi' 'Mexico' 'Nicaragua'
+    #  'NicaraguaSHB' 'Panama' 'Peru' 'PNG' 'Rwanda' 'Tanzania' 'Uganda' 'Yemen' 'Vietnam']
+    # change Costa to Costa Rica, Dom. to Dominican Republic, PNG to Papua New Guinea
+    origins = origins.replace({'Costa': 'Costa Rica', 'Dom.': 'Dominican Republic', 'PNG': 'Papua New Guinea'})
+    # there are some mistakes (e.g. NicaraguaSHB) but they are inevitable
+
+    # map countries to appropriate ids
+    origins = origins.map(origins_map)
+    # drop any mistakes
+    for index, mistake in origins.isna().items():
+        if mistake:
+            origins = origins.drop(index)
+            features = features.drop(index)
+    origins = origins.reset_index(drop=True)
+    features = features.reset_index(drop=True)
+
+    return features, origins
