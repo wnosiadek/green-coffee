00280429fb0fcd2493e89721c4867c73dca225ae
wnosiadek <129866348+wnosiadek@users.noreply.github.com>
2023-05-31 12:01:58 +0200

    Update origin_weight_price.py
    Fixing comments and the output

--- origin_weight_price.py
+++ origin_weight_price.py
@@ -1,7 +1,7 @@
 """
 Script for classifying green coffee origin based on its price and weight
 
-Transforms the data using origins.py, visualizes the data, creates a K-Nearest Neighbors Classifier and fits it on the
+Transforms the data using 'origins.py', visualizes the data, creates a k-Nearest Neighbors Classifier and fits it on the
 training set, tests the model on the testing set
 
 Requires installation of 'pandas', 'matplotlib', 'scikit-learn'
@@ -11,6 +11,8 @@ import pandas
 import origins
 from matplotlib import pyplot
 from sklearn.neighbors import KNeighborsClassifier
+
+print('\nClassifying green coffee origin based on its price and weight...')
 
 # read relevant training data from the adjusted file
 data = pandas.read_csv('adjusted_data.csv')
@@ -22,16 +24,13 @@ test_data = pandas.read_csv('adjusted_test_data.csv')
 test_features = test_data[['Weight', 'Price']]
 test_origins = test_data['Coffee']
 
-# transform training and testing data using origins.py
-# (simplify possible origins,
-#  take into account continents rather than countries,
-#  change alphabetical data into numerical data)
+# transform training and testing data using 'origins.py'
+# (simplify possible origins, take into account continents rather than countries, change alphabetical data into
+#  numerical data)
 train_features, train_origins = origins.simplify_origins(train_features, train_origins)
 test_features, test_origins = origins.simplify_origins(test_features, test_origins)
 # print the mapping for reference
-print()
-print(origins.printable_origins_map)
-# {'Africa': 0, 'Asia & Oceania': 1, 'Mexico & Central America': 2, 'South America': 3}
+print(f'\n{origins.printable_origins_map}')
 
 # plot the data
 pyplot.scatter('Weight', 'Price', data=train_features, c=train_origins, cmap='gist_rainbow')
@@ -48,7 +47,7 @@ train_origins = train_origins.to_numpy()
 test_features = test_features.to_numpy()
 test_origins = test_origins.to_numpy()
 
-# create a k-nearest neighbors classifier for the training data
+# create a k-Nearest Neighbors Classifier for the training data
 classifier = KNeighborsClassifier()
 classifier.fit(train_features, train_origins)
 
@@ -57,10 +56,6 @@ predicted_origins = classifier.predict(test_features)
 accuracy = classifier.score(test_features, test_origins)
 
 # the results
-print()
-print(f'True origins: {test_origins}')
-print(f'Predicted origins: {predicted_origins}')
-print(f'Accuracy: {accuracy:.2f}')
-# True origins: [3 3 3 3 3 2 2 2 3 0 0]
-# Predicted origins: [3. 3. 3. 3. 3. 2. 2. 2. 2. 0. 0.]
-# Accuracy: 0.91
+print(f'\nTrue origins: {test_origins}')
+print(f'Origins predicted with k-Nearest Neighbors: {predicted_origins}')
+print(f'Accuracy (mean accuracy): {accuracy:.2f}')
