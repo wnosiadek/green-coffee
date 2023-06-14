20f5309e9574c9c6d0ee4760b62ff8626cb2c508
wnosiadek <129866348+wnosiadek@users.noreply.github.com>
2023-06-14 17:00:47 +0200

    Update main.py
    Updating info on the results (new testing data)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Main script of the project
 
 Transforms 'data.csv' and 'test_data.csv' using 'adjust_data.py'
-Runs 'price_score.py', 'origin_weight_price.py', 'origin_profile.py', 'process_profile.py', 'process_score_price.py'
+Runs 'price_score.py', 'origin_weight_price.py', 'origin_profile.py', 'process_score_price.py', 'process_profile.py'
 
 Requires installation of 'pandas', 'matplotlib', 'scikit-learn', 'nltk', 'scipy'
 """
@@ -33,9 +33,11 @@ Classifying green coffee origin based on its price and weight...
 
 {'Africa': 0, 'Asia & Oceania': 1, 'Mexico & Central America': 2, 'South America': 3}
 
-True origins: [3 3 3 3 3 2 2 2 3 0 0]
-Origins predicted with Decision Trees: [3. 3. 3. 3. 3. 3. 2. 2. 3. 0. 0.]
-Accuracy (mean accuracy): 0.91
+True origins: [3. 3. 3. 3. 3. 3. 2. 0. 0. 0. 2. 2. 2. 2. 2. 2. 1. 1. 2. 2. 2. 2. 2. 2. 2. 2. 2.
+               2. 2. 0. 0. 0. 0. 0. 0. 0. 0.]
+Origins predicted with Decision Trees: [3. 3. 3. 3. 3. 3. 2. 0. 0. 0. 2. 2. 3. 2. 2. 3. 0. 3. 2. 2. 2. 3. 2. 2. 3. 2. 2.
+                                        2. 2. 0. 0. 0. 0. 0. 0. 1. 0.]
+Accuracy (mean accuracy): 0.81
 '''
 # pop-up plots: 'origin_weight_price.png', 'origin_weight_price_tree.png'
 
@@ -47,26 +49,13 @@ Classifying green coffee origin based on its sensory profile...
 
 {'Africa': 0, 'Asia & Oceania': 1, 'Mexico & Central America': 2, 'South America': 3}
 
-True origins: [3 3 3 3 3 2 3 0 0]
-Origins predicted with Multinomial Naive Bayes: [3. 3. 2. 3. 3. 3. 3. 0. 0.]
-Accuracy (mean accuracy): 0.78
-
-True origins: [3 3 3 3 3 2 3 0 0]
-Origins predicted with Complement Naive Bayes: [3. 3. 2. 2. 2. 3. 3. 0. 0.]
+True origins: [3 3 3 2 2 2 2 1 1 2 2 0 0 0 0 0]
+Origins predicted with Multinomial Naive Bayes: [3. 3. 3. 0. 2. 3. 3. 2. 3. 2. 3. 0. 0. 0. 0. 3.]
 Accuracy (mean accuracy): 0.56
-'''
 
-import process_profile
-
-# output of 'process_profile.py'
-'''
-Classifying green coffee processing method based on its sensory profile...
-
-{'washed': 0, 'natural': 1, 'pulped natural': 2, 'wet hulled': 3, 'anaerobic': 4}
-
-True processing methods: [2 1 1 0 0 0 0 0 1]
-Processing methods predicted with Support Vector Machines: [0. 0. 1. 0. 0. 0. 0. 1. 0.]
-Accuracy (mean accuracy): 0.56
+True origins: [3 3 3 2 2 2 2 1 1 2 2 0 0 0 0 0]
+Origins predicted with Complement Naive Bayes: [3. 2. 2. 2. 2. 2. 3. 2. 1. 2. 3. 0. 0. 0. 0. 1.]
+Accuracy (mean accuracy): 0.62
 '''
 
 import process_score_price
@@ -77,12 +66,25 @@ Classifying green coffee processing method based on its price and score...
 
 {'washed': 0, 'natural': 1, 'pulped natural': 2, 'anaerobic': 3}
 
-True processing methods: [2 0 1]
-Processing methods predicted with k-Nearest Neighbors: [1. 1. 0.]
-Accuracy (mean accuracy): 0.00
+True processing methods: [1 0 0 0 0 1 0 0 0]
+Processing methods predicted with k-Nearest Neighbors: [1. 0. 0. 0. 1. 0. 0. 1. 0.]
+Accuracy (mean accuracy): 0.67
 
-True processing methods: [2 0 1]
-Processing methods predicted with Fixed-Radius Near Neighbors: [0. 1. 0.]
-Accuracy (mean accuracy): 0.00
+True processing methods: [1 0 0 0 0 1 0 0 0]
+Processing methods predicted with Fixed-Radius Near Neighbors: [1. 0. 0. 1. 1. 0. 0. 0. 0.]
+Accuracy (mean accuracy): 0.67
 '''
 # pop-up plot: 'process_score_price.png'
+
+import process_profile
+
+# output of 'process_profile.py'
+'''
+Classifying green coffee processing method based on its sensory profile...
+
+{'washed': 0, 'natural': 1, 'pulped natural': 2, 'wet hulled': 3, 'anaerobic': 4}
+
+True processing methods: [1. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0.]
+Processing methods predicted with Support Vector Machines: [0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 1. 0. 0. 0. 0.]
+Accuracy (mean accuracy): 0.67
+'''
